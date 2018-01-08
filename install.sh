ff2f23091e83c9f0d1ea9c72aba6aea2e414b517
greymd <greengregson@gmail.com>
2018-01-08 18:26:22 +0900

    [skip ci] Set logs to (un)install.sh

--- install.sh
+++ install.sh
@@ -10,10 +10,14 @@ readonly PREFIX_MAN="${PREFIX}/share/man/man1"
 
 # Install (bin)
 mkdir -p "${PREFIX_BIN}"
+echo install -m "${BINMODE}" "${THIS_DIR}/bin/xpanes" "${PREFIX_BIN}/xpanes"
 install -m "${BINMODE}" "${THIS_DIR}/bin/xpanes" "${PREFIX_BIN}/xpanes"
+echo install -m "${BINMODE}" "${THIS_DIR}/bin/tmux-xpanes" "${PREFIX_BIN}/tmux-xpanes"
 install -m "${BINMODE}" "${THIS_DIR}/bin/tmux-xpanes" "${PREFIX_BIN}/tmux-xpanes"
 
 # Install (man)
 mkdir -p "${PREFIX_MAN}"
+echo install -m "${BINMODE}" "${THIS_DIR}/man/xpanes.1" "${PREFIX_MAN}/xpanes.1"
 install -m "${BINMODE}" "${THIS_DIR}/man/xpanes.1" "${PREFIX_MAN}/xpanes.1"
+echo install -m "${BINMODE}" "${THIS_DIR}/man/tmux-xpanes.1" "${PREFIX_MAN}/tmux-xpanes.1"
 install -m "${BINMODE}" "${THIS_DIR}/man/tmux-xpanes.1" "${PREFIX_MAN}/tmux-xpanes.1"
--- uninstall.sh
+++ uninstall.sh
@@ -7,9 +7,13 @@ readonly PREFIX_BIN="${PREFIX}/bin"
 readonly PREFIX_MAN="${PREFIX}/share/man/man1"
 
 # Uninstall (bin)
+echo rm -f "${PREFIX_BIN}/xpanes"
 rm -f "${PREFIX_BIN}/xpanes"
+echo rm -f "${PREFIX_BIN}/tmux-xpanes"
 rm -f "${PREFIX_BIN}/tmux-xpanes"
 
 # Uninstall (man)
+echo rm -f "${PREFIX_MAN}/xpanes.1"
 rm -f "${PREFIX_MAN}/xpanes.1"
+echo rm -f "${PREFIX_MAN}/tmux-xpanes.1"
 rm -f "${PREFIX_MAN}/tmux-xpanes.1"
