67657530db8e52d68257bf46fc643122b64dda90
greymd <yamadagrep@gmail.com>
2020-05-04 15:40:33 +0100

    [skip ci] Apply shfmt

--- activate.sh
+++ activate.sh
@@ -2,7 +2,7 @@
 
 # Let PATH include 'xpanes' and 'tmux-xpanes'.
 if ! (type xpanes > /dev/null 2>&1 &&
-  type      tmux-xpanes > /dev/null 2>&1); then
+  type     tmux-xpanes > /dev/null 2>&1); then
   __XPANES_DIR="$(  cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)"
   export   PATH="$PATH:$__XPANES_DIR/bin"
 fi
