887b2d7d60d31fb7fa63abb3ee6d79a9ecfe4fbf
Isidro Arias <isidroariass@hotmail.es>
2025-02-18 20:18:57 +0100

    avoid duplication in install.sh
    It will show something like:
    
    $ sudo ./install.sh
    + install -d /usr/local/bin
    + install -m 755 /tmp/tmux-xpanes/bin/xpanes /usr/local/bin/xpanes
    + install -m 755 /tmp/tmux-xpanes/bin/tmux-xpanes /usr/local/bin/tmux-xpanes
    + install -d /usr/local/share/man/man1
    + install -m 644 /tmp/tmux-xpanes/man/xpanes.1 /usr/local/share/man/man1/xpanes.1
    + install -m 644 /tmp/tmux-xpanes/man/tmux-xpanes.1 /usr/local/share/man/man1/tmux-xpanes.1

--- install.sh
+++ install.sh
@@ -9,18 +9,14 @@ readonly PREFIX="${1:-/usr/local}"
 readonly PREFIX_BIN="${PREFIX}/bin"
 readonly PREFIX_MAN="${PREFIX}/share/man/man1"
 
+set -x
+
 # Install (bin)
-echo install -d "${PREFIX_BIN}"
 install -d "${PREFIX_BIN}"
-echo install -m "${BINMODE}" "${THIS_DIR}/bin/xpanes" "${PREFIX_BIN}/xpanes"
 install -m "${BINMODE}" "${THIS_DIR}/bin/xpanes" "${PREFIX_BIN}/xpanes"
-echo install -m "${BINMODE}" "${THIS_DIR}/bin/tmux-xpanes" "${PREFIX_BIN}/tmux-xpanes"
 install -m "${BINMODE}" "${THIS_DIR}/bin/tmux-xpanes" "${PREFIX_BIN}/tmux-xpanes"
 
 # Install (man)
-echo install -d "${PREFIX_MAN}"
 install -d "${PREFIX_MAN}"
-echo install -m "${DOCMODE}" "${THIS_DIR}/man/xpanes.1" "${PREFIX_MAN}/xpanes.1"
 install -m "${DOCMODE}" "${THIS_DIR}/man/xpanes.1" "${PREFIX_MAN}/xpanes.1"
-echo install -m "${DOCMODE}" "${THIS_DIR}/man/tmux-xpanes.1" "${PREFIX_MAN}/tmux-xpanes.1"
 install -m "${DOCMODE}" "${THIS_DIR}/man/tmux-xpanes.1" "${PREFIX_MAN}/tmux-xpanes.1"
